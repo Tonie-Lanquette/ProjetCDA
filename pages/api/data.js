26cf1cc780a2c1df5f90a51569b0e6644e670b16
Tonie <tonie.lanquette.simplon@gmail.com>
2024-08-02 13:08:49 +0200

    appel api dans index ok

--- pages/api/data.js
+++ pages/api/data.js
@@ -1,8 +1,12 @@
-export default async function handler(req, res) {
-  const { cityInput } = req.body;
-  const getWeatherData = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric&appid=${process.env.OPENWEATHER_API_KEY}`
-  );
-  const data = await getWeatherData.json();
-  res.status(200).json(data);
-}
+// import cities from "../../city.json";
+
+// export default async function handler(req, res) {
+//   const { latitude } = city;
+//   const { longitude } = city;
+//   const getWeatherData = await fetch(
+//     `https://api.open-meteo.com/v1/forecast?latitude=${cities.latitude}&longitude=${cities.longitude}&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,rain,showers,snowfall,weather_code,wind_speed_10m,wind_direction_10m&daily=sunrise,sunset&models=meteofrance_arome_france`
+    
+//   );
+//   const data = await getWeatherData.json();
+//   res.status(200).json(data);
+// }
--- pages/index.js
+++ pages/index.js
@@ -4,63 +4,64 @@ import { MainCard } from "../components/MainCard";
 import { ContentBox } from "../components/ContentBox";
 import { Header } from "../components/Header";
 import { DateAndTime } from "../components/DateAndTime";
-import { Search } from "../components/Search";
 import { MetricsBox } from "../components/MetricsBox";
 import { UnitSwitch } from "../components/UnitSwitch";
 import { LoadingScreen } from "../components/LoadingScreen";
 import { ErrorScreen } from "../components/ErrorScreen";
 
 import styles from "../styles/Home.module.css";
+import { cities }  from "../city.json";
 
 export const App = () => {
-  const [cityInput, setCityInput] = useState("Riga");
-  const [triggerFetch, setTriggerFetch] = useState(true);
   const [weatherData, setWeatherData] = useState();
   const [unitSystem, setUnitSystem] = useState("metric");
 
-  useEffect(() => {
-    const getData = async () => {
-      const res = await fetch("api/data", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ cityInput }),
-      });
-      const data = await res.json();
-      setWeatherData({ ...data });
-      setCityInput("");
+ const  getData = async () => {
+      const response = await fetch(
+        `https://api.open-meteo.com/v1/forecast?latitude=${cities.latitude}&longitude=${cities.longitude}&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,rain,showers,snowfall,weather_code,wind_speed_10m,wind_direction_10m&daily=sunrise,sunset&models=meteofrance_arome_france`
+      );
+
+      const data = await response.json();
+      setWeatherData(data);
+      console.log(data)
     };
+
+  useEffect(() => {
     getData();
-  }, [triggerFetch]);
+  }, []);
 
-  const changeSystem = () =>
-    unitSystem == "metric"
-      ? setUnitSystem("imperial")
-      : setUnitSystem("metric");
+ 
 
-  return weatherData && !weatherData.message ? (
-    <div className={styles.wrapper}>
-      <MainCard
-        city={weatherData.name}
-        country={weatherData.sys.country}
-        description={weatherData.weather[0].description}
-        iconName={weatherData.weather[0].icon}
-        unitSystem={unitSystem}
-        weatherData={weatherData}
-      />
-      <ContentBox>
-        <Header>
-          <DateAndTime weatherData={weatherData} unitSystem={unitSystem} />
-        </Header>
-        <MetricsBox weatherData={weatherData} unitSystem={unitSystem} />
-        <UnitSwitch onClick={changeSystem} unitSystem={unitSystem} />
-      </ContentBox>
-    </div>
-  ) : weatherData && weatherData.message ? (
-    <ErrorScreen errorMessage="City not found, try again!">
-    </ErrorScreen>
-  ) : (
-    <LoadingScreen loadingMessage="Loading data..." />
-  );
-};
+//   const changeSystem = () =>
+//     unitSystem == "metric"
+//       ? setUnitSystem("imperial")
+//       : setUnitSystem("metric");
+
+//   return weatherData && !weatherData.message ? (
+//     <div className={styles.wrapper}>
+//       <MainCard
+//         city={cities.name}
+//         country={cities .country}
+//         // description={weatherData.}
+//         weatherCode={weatherData.current.weather_code}
+//         // iconName={weatherData.weather[0].icon}
+//         unitSystem={unitSystem}
+//         weatherData={weatherData}
+//       />
+//       <ContentBox>
+//         <Header>
+//           <DateAndTime weatherData={weatherData} unitSystem={unitSystem} />
+//         </Header>
+//         <MetricsBox weatherData={weatherData} unitSystem={unitSystem} />
+//         <UnitSwitch onClick={changeSystem} unitSystem={unitSystem} />
+//       </ContentBox>
+//     </div>
+//   ) : weatherData && weatherData.message ? (
+//     <ErrorScreen errorMessage="City not found, try again!">
+//     </ErrorScreen>
+//   ) : (
+//     <LoadingScreen loadingMessage="Loading data..." />
+//   );
+ };
 
 export default App;
