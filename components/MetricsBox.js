4fe6cdcc2cca75cb8b7fe0dd55c876d45a1ed785
Tonie <tonie.lanquette.simplon@gmail.com>
2024-08-02 15:36:06 +0200

    affichage data ok exept date/hours

--- components/MetricsBox.js
+++ components/MetricsBox.js
@@ -14,24 +14,24 @@ export const MetricsBox = ({ weatherData, unitSystem }) => {
       <MetricsCard
         title={"Humidity"}
         iconSrc={"/icons/humidity.png"}
-        metric={weatherData.main.humidity}
+        metric={weatherData.current.relative_humidity_2m}
         unit={"%"}
       />
       <MetricsCard
         title={"Wind speed"}
         iconSrc={"/icons/wind.png"}
-        metric={getWindSpeed(unitSystem, weatherData.wind.speed)}
+        metric={getWindSpeed(unitSystem, weatherData.current.wind_speed_10m)}
         unit={unitSystem == "metric" ? "m/s" : "m/h"}
       />
       <MetricsCard
         title={"Wind direction"}
         iconSrc={"/icons/compass.png"}
-        metric={degToCompass(weatherData.wind.deg)}
+        metric={degToCompass(weatherData.current.wind_direction_10m)}
       />
       <MetricsCard
         title={"Visibility"}
         iconSrc={"/icons/binocular.png"}
-        metric={getVisibility(unitSystem, weatherData.visibility)}
+        metric={getVisibility(unitSystem, weatherData.hourly_units.visibility)}
         unit={unitSystem == "metric" ? "km" : "miles"}
       />
       <MetricsCard
@@ -39,12 +39,12 @@ export const MetricsBox = ({ weatherData, unitSystem }) => {
         iconSrc={"/icons/sunrise.png"}
         metric={getTime(
           unitSystem,
-          weatherData.sys.sunrise,
+          weatherData.daily_units.sunrise,
           weatherData.timezone
         )}
         unit={getAMPM(
           unitSystem,
-          weatherData.sys.sunrise,
+          weatherData.daily_units.sunrise,
           weatherData.timezone
         )}
       />
@@ -53,10 +53,10 @@ export const MetricsBox = ({ weatherData, unitSystem }) => {
         iconSrc={"/icons/sunset.png"}
         metric={getTime(
           unitSystem,
-          weatherData.sys.sunset,
+          weatherData.daily_units.sunset,
           weatherData.timezone
         )}
-        unit={getAMPM(unitSystem, weatherData.sys.sunset, weatherData.timezone)}
+        unit={getAMPM(unitSystem, weatherData.daily_units.sunset, weatherData.timezone)}
       />
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export const App = () => {
 
  const  getData = async () => {
       const response = await fetch(
-        `https://api.open-meteo.com/v1/forecast?latitude=${cities.latitude}&longitude=${cities.longitude}&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,rain,showers,snowfall,weather_code,wind_speed_10m,wind_direction_10m&daily=sunrise,sunset&models=meteofrance_arome_france`
+        `https://api.open-meteo.com/v1/forecast?latitude=${cities.latitude}&longitude=${cities.longitude}&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,rain,showers,snowfall,weather_code,wind_speed_10m,wind_direction_10m&hourly=visibility&daily=sunrise,sunset&models=meteofrance_arome_france`
       );
 
       const data = await response.json();
@@ -32,36 +32,36 @@ export const App = () => {
 
  
 
-//   const changeSystem = () =>
-//     unitSystem == "metric"
-//       ? setUnitSystem("imperial")
-//       : setUnitSystem("metric");
+  const changeSystem = () =>
+    unitSystem == "metric"
+      ? setUnitSystem("imperial")
+      : setUnitSystem("metric");
 
-//   return weatherData && !weatherData.message ? (
-//     <div className={styles.wrapper}>
-//       <MainCard
-//         city={cities.name}
-//         country={cities .country}
-//         // description={weatherData.}
-//         weatherCode={weatherData.current.weather_code}
-//         // iconName={weatherData.weather[0].icon}
-//         unitSystem={unitSystem}
-//         weatherData={weatherData}
-//       />
-//       <ContentBox>
-//         <Header>
-//           <DateAndTime weatherData={weatherData} unitSystem={unitSystem} />
-//         </Header>
-//         <MetricsBox weatherData={weatherData} unitSystem={unitSystem} />
-//         <UnitSwitch onClick={changeSystem} unitSystem={unitSystem} />
-//       </ContentBox>
-//     </div>
-//   ) : weatherData && weatherData.message ? (
-//     <ErrorScreen errorMessage="City not found, try again!">
-//     </ErrorScreen>
-//   ) : (
-//     <LoadingScreen loadingMessage="Loading data..." />
-//   );
+  return weatherData && !weatherData.message ? (
+    <div className={styles.wrapper}>
+      <MainCard
+        city={cities.name}
+        country={cities.country}
+        // description={weatherData.}
+        weatherCode={weatherData.current.weather_code}
+        // iconName={weatherData.weather[0].icon}
+        unitSystem={unitSystem}
+        weatherData={weatherData}
+      />
+      <ContentBox>
+        <Header>
+          <DateAndTime weatherData={weatherData} unitSystem={unitSystem} />
+        </Header>
+        <MetricsBox weatherData={weatherData} unitSystem={unitSystem} />
+        <UnitSwitch onClick={changeSystem} unitSystem={unitSystem} />
+      </ContentBox>
+    </div>
+  ) : weatherData && weatherData.message ? (
+    <ErrorScreen errorMessage="City not found, try again!">
+    </ErrorScreen>
+  ) : (
+    <LoadingScreen loadingMessage="Loading data..." />
+  );
  };
 
 export default App;
